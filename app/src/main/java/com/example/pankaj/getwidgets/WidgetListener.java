14d0da1ab49ca39e05126a6c4a5efac5dcd868ba
shamoorpanik1 <shamoorpanik1@gmail.com>
2018-02-19 14:39:50 +0530

    commit

---
+++ app/src/main/java/com/example/pankaj/getwidgets/WidgetListener.java
@@ -0,0 +1,10 @@
+package com.example.pankaj.getwidgets;
+
+import android.view.View;
+
+/**
+ * Created by dag on 12/02/15.
+ */
+public interface WidgetListener {
+	void onLongpress (View view);
+}
